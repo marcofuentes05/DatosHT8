fe1a7d66a26f5188c2c61440ec05684feefe21f2
Marco Fuentes <marcofuentes1607@gmail.com>
2019-04-01 21:14:13 -0600

    JUnit creado

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,66 @@
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
 
 public class Main {
-
     public static void main (String args[]){
-
         try{
             VectorHeap<Paciente> queue = new VectorHeap<>();
 
-            String sCadena ="";
+            String sCadena;
             BufferedReader bf = new BufferedReader(new FileReader("pacientes.txt"));
             while ((sCadena = bf.readLine())!=null) {
                 String[] px = sCadena.split(",");
                 queue.add(new Paciente(px[0],px[1],px[2]));
             }
-            System.out.println(queue.remove().toString());
-            System.out.println(queue.remove().toString());
-            System.out.println(queue.remove().toString());
-            System.out.println(queue.remove().toString());
+
+            String menu = "             Hospital \n" +
+                          " 1. Ver el siguiente paciente en la sala de espera\n" +
+                          " 2. Agregar un nuevo paciente \n" +
+                          " 3. Cerrar por hoy";
+            String respuesta ="";
+            while (!respuesta.equals("3")){
+                System.out.println(menu);
+                Scanner teclado = new Scanner(System.in);
+                respuesta=teclado.nextLine();
+                switch (respuesta) {
+                    case "1":
+                        System.out.println("\n3" + queue.remove().toString() + "\n");
+                        break;
+                    case "2":
+                        System.out.println("Nombre del paciente: ");
+                        Scanner r = new Scanner(System.in);
+                        String nombre = r.nextLine();
+
+                        System.out.println("Sintoma: ");
+                        Scanner j = new Scanner(System.in);
+                        String sintoma = j.nextLine();
+
+                        System.out.println("Prioridad: ");
+                        Scanner k = new Scanner(System.in);
+                        String prioridad = k.nextLine();
+
+                        queue.add(new Paciente(nombre, sintoma, prioridad));
+                        break;
+                    case "3":
+                        System.out.println("Hasta luego!");
+                        break;
+                    default:
+                        System.out.println("Ese dato no es válido");
+                        break;
+                }
+            }
         }catch(Exception e){
-            e.printStackTrace();
+            if(e instanceof NoSuchElementException){
+                System.out.println("Ya no hay mas pacientes! :D");
+            }
+            else if (e instanceof FileNotFoundException){
+                System.out.println("No se pudo encontrar el archivo o hay problemas con este....");
+            }else{
+                System.out.println("Algo salio mal... :(");
+            }
         }
-
-
     }
-
 }
---
+++ src/VectorHeapTest.java
@@ -0,0 +1,14 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class VectorHeapTest {
+
+    @org.junit.jupiter.api.Test
+    void add() {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void remove() {
+
+    }
+}
